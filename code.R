7351fb3f16bc51c04f73756887898dc0d0ce5e24
Lucy <ld.mcgowan@vanderbilt.edu>
2017-03-18 14:31:56 -0500

    fix json

--- code.R
+++ code.R
@@ -10,5 +10,15 @@ dat_df %>%
   summarise_each(funs(list)) %>% 
   toJSON(pretty=TRUE) -> dat_json
 
+dat_json <- gsub('\\[\\"', '\\"',dat_json)
+dat_json <- gsub('\\"\\]', '\\"',dat_json)
+dat_json <- gsub('\\[false\\]',"false",dat_json)
+dat_json <- gsub('\\[null\\]',"null",dat_json)
+dat_json <- gsub('\\[true\\]',"true",dat_json)
+dat_json <- gsub('\\.latitude',"_latitude",dat_json)
+dat_json <- gsub('\\.longitude',"_longitude",dat_json)
+dat_json <- gsub('\\.needs_recording',"_needs_recording",dat_json)
+dat_json <- gsub('\\.human_address',"_human_address",dat_json)
+
 write(dat_json,file = "park_dat_json.txt")
 write.csv(dat_df, file = "park_dat.csv")
